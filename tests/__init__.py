6132c36796f3c4f141965677b0ffda630bd5153b
Ryder <dev@ryderdamen.com>
2018-11-27 19:29:37 -0500

    adding in join from os.path

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 import sys
-from os.path import dirname, realpath
+from os.path import dirname, realpath, join
 root = dirname(dirname(realpath(__file__)))
 sys.path.append(root)
-sys.path.append(os.path.join(root, 'phonetic_alphabet'))
+sys.path.append(join(root, 'phonetic_alphabet'))
